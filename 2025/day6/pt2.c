6d89d99572da33c91f44aa6ebad5896c32cc9e2b
Andreas Bjørgulv Nore <andreas.nore@gmail.com>
2025-12-07 00:12:38 +0100

    Fixed some comments and syntax. Done

--- 2025/day6/pt2.c
+++ 2025/day6/pt2.c
@@ -6,13 +6,13 @@
 
 int pt2(FILE* fp)
 {
-    /* Reading file into buffer, and getting widht and height on table*/
+    /* Reading file into buffer, and getting width and height*/
     char buffer[32000];
     size_t pos = 0;
 
     int c;
     int w=0,h=0,n=0;
-    int num_space;
+
     while ((c = fgetc(fp)) != EOF) {
         if(c=='\n'){
             if(h==0)w=n;
@@ -25,9 +25,9 @@ int pt2(FILE* fp)
     buffer[pos] = '\0';
 
     /*
-        Pt2 its different, now i have to start at the end ( wish i had tac )
-        and construct the numbers, one char at a time, start at the top, and going down
-        the column. Therefore at the most right its 0 0 4 as one num, and then 4 3 1 etc
+     Pt2 its different, now i have to start at the end ( wish i had tac )
+     and construct the numbers, one char at a time, start at the top, and going down
+     the column. Therefore at the most right its 0 0 4 as one num, and then 4 3 1 etc
 
         123 328  51 64-
          45 64  387 23-
@@ -38,27 +38,27 @@ int pt2(FILE* fp)
                      40
                      30
                      14
-        The rightmost problem is 4 + 431 + 623 = 1058
+      The rightmost problem is 4 + 431 + 623 = 1058
 
-        First its idx 0 for each line, so thats l[0], l[1], etc. which equates to
-        num idx+w*0, idx+w*1, idx+w*2 etc.. Idx is the inrow ref, and w*x is the column
-     */
-    /* Now the buffer comes in handy! I also saved the \n chars, so i will count from
-     * the end of the line, work my way backwards, and the top num is MSC, therefore
-     i should be able to just construct the num straight into an int
-     remember counting is 1 based, so w-1 for column num
-     */
+      First its idx 0 for each line, so thats l[0], l[1], etc. which equates to
+      num idx+w*0, idx+w*1, idx+w*2 etc.. Idx is the inrow ref, and w*x is the column
+    
+      Now the buffer comes in handy! so i will count from the end of the line,
+      work my way backwards, and the top num is MSC, therefore i should be able
+      to just construct the num straight into an int remember counting is 1
+      based, so w-1 for column num etc*/
+
     int op_row = h - 1;          // operator row
     int num_rows = h - 1;        // rows containing digits
+    int num_cols = w - 1;        // rows containing digits
     int num_count = 0;
     char digits[128]={0}, op;         // container for the nums, and operations
     uint64_t stack[128]={0};
 
     uint64_t final_score=0;
 
-    for(int col=w-1; col>=0; --col) // This is every char (col), starting at the end
+    for(int col=num_cols; col>=0; --col) // This is every char (col), starting at the end
     {
-        TRACE("col %i out of %i", col, w);
         int dpos = 0;  // index for collecting digits only
 
         /* Forst row 0, column 15, then we move to
@@ -77,23 +77,28 @@ int pt2(FILE* fp)
         if (dpos > 0) {
             uint64_t value = strtoull(digits, NULL, 10);
             stack[num_count++] = value;
-            DEBUG("col=%i -> number %llu", col, value);
+            TRACE("col=%i -> number %llu", col, value);
         }        
 
-        op = buffer[w*op_row+col];
+        op = buffer[w*op_row+col]; // Extracting the operator at the end
 
         // The operators is the marker for the sequence being done!
         if ((op == '+' || op == '*') && num_count > 0) {
 
             uint64_t acc = stack[0];
             for (int i = 1; i < num_count; i++) {
-                if (op == '+')
+                if (op == '+') {
+                    TRACE("%llu + %llu = %llu", stack[i-1], stack[i], (stack[i-1]+stack[i]));
                     acc += stack[i];
-                else
+                }
+                else {
+                    TRACE("%llu * %llu = %llu", stack[i-1], stack[i], (stack[i-1]*stack[i]));
                     acc *= stack[i];
+                }
             }
 
-            INFO("Problem result (%c): %llu", op, acc);
+            DEBUG("Problem result (%c): %llu", op, acc);
+            DEBUG("+ %llu = %llu", final_score, final_score+acc);
             final_score += acc;
 
             num_count = 0; // reset for next group
