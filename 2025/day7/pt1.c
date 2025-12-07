21132f6ea8746694ce942009e2cd58d00f359603
Andreas Bjørgulv Nore <andreas.nore@gmail.com>
2025-12-07 19:12:29 +0100

    Part 1 of day7 done - short and sweet

---
+++ 2025/day7/pt1.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <blackbox.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int pt1(FILE* fp){
+
+    int split=0;
+    ssize_t line_len=0;
+    int start_i = 0, split_i=0;
+    char *curr_line = NULL;     // reading in to current line
+    char *prev_line = NULL;     // remembering the previous line
+    char ch;                    // used for searching 
+    size_t cap = 0;             // capacity for line (how much allocated)
+
+    // and i know this technically leaks memory, but who cares
+    // Read in the first line and find the 'S'
+
+    while((line_len = getline(&curr_line, &cap, fp)) > 0)
+    {
+        if(curr_line[line_len-1]=='\n') {
+            curr_line[line_len-1]='\0';
+        }
+
+        if(prev_line == NULL){
+            DEBUG("line length=%zu, including \\n", line_len);
+
+            prev_line = realloc(prev_line, sizeof(char)*line_len);
+            while((ch=curr_line[start_i])!='S') {
+                start_i++;
+            }
+
+            TRACE("S is at index [%i] in curr_line: %c", start_i, curr_line[start_i]);
+
+            memcpy(prev_line, curr_line, line_len); 
+            continue;
+            /* The first line starts as a previous line */
+        }         
+        
+        if(start_i){
+            curr_line[start_i] = '|';
+            start_i = 0;
+        } else {
+            // Here is where we will look for the ^ and start splitting and counting
+            for(int i=0; i<line_len;++i){
+                if(prev_line[i]=='|' && curr_line[i]=='^')
+                {
+                    if(i>0) curr_line[i-1]='|';
+                    if(i<line_len-1) curr_line[i+1]='|';
+                    split++;
+                } else
+                if(prev_line[i]=='|' && curr_line[i]=='.')
+                {
+                    curr_line[i]='|';
+                }
+            }
+        }
+        TRACE("The previous line is %s", prev_line);
+        DEBUG("The current line is %s", curr_line);
+
+        memcpy(prev_line, curr_line, line_len); 
+    }
+
+
+    INFO("Amount of time we split is %i", split);
+
+    return 0;
+}
