c6f9e59f6f7db062eeff372ea6a22cdf7f879249
Andreas Bjørgulv Nore <andreas.nore@gmail.com>
2025-12-30 00:58:54 +0100

    Well, didnt need to be that fancy even

--- 2015/day04/pt1.c
+++ 2015/day04/pt1.c
@@ -3,11 +3,6 @@
 #include <string.h>
 #include <CommonCrypto/CommonCrypto.h>
 
-static const char *calc_md5(unsigned char *digest, const char *str) {
-    CC_MD5(str, (CC_LONG)strlen(str), digest);
-    return str;
-}
-
 int pt1(const char *str) {
 
     char string[33]={0};
@@ -15,7 +10,7 @@ int pt1(const char *str) {
 
     for(int i=0; i<INT_MAX; ++i){
         sprintf(string, "%s%d", str,i);
-        calc_md5(digest, string);
+        CC_MD5(string, (CC_LONG)strlen(string), digest);
         if (!digest[0] && !digest[1] &&
             (digest[2] & 0xF0) == 0x00) // mask out the first nibble
             return i;
--- 2015/day04/pt2.c
+++ 2015/day04/pt2.c
@@ -3,11 +3,6 @@
 #include <string.h>
 #include <CommonCrypto/CommonCrypto.h>
 
-static const char *calc_md5(unsigned char *digest, const char *str) {
-    CC_MD5(str, (CC_LONG)strlen(str), digest);
-    return str;
-}
-
 int pt2(const char *str)
 {
     char string[33]={0};
@@ -15,7 +10,7 @@ int pt2(const char *str)
 
     for(int i=0; i<INT_MAX; ++i){
         sprintf(string, "%s%d", str,i);
-        calc_md5(digest, string);
+        CC_MD5(string, (CC_LONG)strlen(string), digest);
         if (!digest[0] && !digest[1] && !digest[2])
             return i;
     }
